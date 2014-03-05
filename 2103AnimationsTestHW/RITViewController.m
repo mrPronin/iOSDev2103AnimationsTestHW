27c553875e70eba361729888d95ac61deea40991
Alexander Pronin <pronin.alx@gmail.com>
2014-03-05 21:55:21 +0200

    First commit

--- 2103AnimationsTestHW/RITViewController.m
+++ 2103AnimationsTestHW/RITViewController.m
@@ -20,10 +20,73 @@
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
+- (void) viewDidAppear:(BOOL)animated {
+    
+    [super viewDidAppear:animated];
+    
+    [self moveView:self.views withClockwiseDirection:arc4random() % 2];
+}
+
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Helper methosd
+
+- (void) moveView:(NSArray*) views withClockwiseDirection:(BOOL) clockwiseDirection {
+    
+    [UIView animateWithDuration:3.f
+                          delay:0
+                        options:UIViewAnimationOptionCurveEaseInOut
+                     animations:^{
+                         
+                         UIView* firstView = views[0];
+                         UIView* lastView = views[[views count] - 1];
+                         
+                         if (clockwiseDirection) {
+                             
+                             CGPoint center = firstView.center;
+                             UIColor* color = firstView.backgroundColor;
+                             
+                             for (int i = 0; i < [views count] - 1; i++) {
+                                 UIView* view = views[i];
+                                 UIView* nextView = views[i + 1];
+                                 
+                                 view.backgroundColor = nextView.backgroundColor;
+                                 view.center = nextView.center;
+                                 
+                             }
+                             
+                             lastView.backgroundColor = color;
+                             lastView.center = center;
+                             
+                         } else {
+                             
+                             CGPoint center = lastView.center;
+                             UIColor* color = lastView.backgroundColor;
+                             
+                             for (int i = [views count] - 1; i > 0; i--) {
+                                 UIView* view = views[i];
+                                 UIView* previousView = views[i - 1];
+                                 
+                                 view.backgroundColor = previousView.backgroundColor;
+                                 view.center = previousView.center;
+                             }
+                             
+                             firstView.backgroundColor = color;
+                             firstView.center = center;
+                         }
+                         
+                     }
+                     completion:^(BOOL finished) {
+                         
+                         __weak NSArray* v = views;
+                         [self moveView:v withClockwiseDirection:arc4random() % 2];
+                         
+                     }];
+    
 }
 
 @end
