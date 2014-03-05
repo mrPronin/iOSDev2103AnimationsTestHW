91e5a52ffc583b64f9ebf229a2d6e446abc0498a
Alexander Pronin <pronin.alx@gmail.com>
2014-03-05 21:46:49 +0200

    Initial Commit

---
+++ 2103AnimationsTestHW/RITAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RITAppDelegate.h
+//  2103AnimationsTestHW
+//
+//  Created by Pronin Alexander on 05.03.14.
+//  Copyright (c) 2014 Pronin Alexander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RITAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
